7c7c8c5dfd100b44914f00d522d43801d88fb236
Owen Johnson <owen@owenjohnson.info>
2015-05-04 16:47:47 -0500

    Use C# 6.0 features

--- LittleBraces/BraceLineTransformSource.cs
+++ LittleBraces/BraceLineTransformSource.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.ComponentModel.Composition;
-using System.Diagnostics;
-using System.IO;
-using System.Reflection;
+﻿using System.ComponentModel.Composition;
 using System.Text.RegularExpressions;
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Text.Formatting;
@@ -16,10 +12,7 @@ namespace BraceLineShrinker
     [TextViewRole(PredefinedTextViewRoles.Document)]
     public class BraceLineTransformSourceProvider : ILineTransformSourceProvider
     {
-        public ILineTransformSource Create(IWpfTextView textView)
-        {
-            return new BraceLineTransformSource();
-        }
+        public ILineTransformSource Create(IWpfTextView textView) => new BraceLineTransformSource();
     }
 
     public class BraceLineTransformSource : ILineTransformSource
@@ -27,14 +20,12 @@ namespace BraceLineShrinker
         /// <summary>
         /// Scale factor of brace lines. Expose for another extension to change it if needed.
         /// </summary>
-        public static double? BraceLineScale { get; set; }
-        public static Regex BraceMatchExpression = new Regex(@"^\s*(\{|\};?|\s)*\s*$");
+        public static double BraceLineScale { get; set; } = .45;
+        public static Regex BraceMatchExpression { get; set; } = new Regex(@"^\s*(\{|\};?|\s)*\s*$");
 
         public LineTransform GetLineTransform(ITextViewLine line, double yPosition, ViewRelativePosition placement)
         {
-            if (BraceMatchExpression.IsMatch(line.Extent.GetText()))
-                return new LineTransform(BraceLineScale ?? .45);
-            return line.DefaultLineTransform;
+            return BraceMatchExpression.IsMatch(line.Extent.GetText()) ? new LineTransform(BraceLineScale) : line.DefaultLineTransform;
         }
     }
 }
